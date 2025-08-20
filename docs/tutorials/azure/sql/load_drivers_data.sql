7e778fab4ed2b8edea51a07711d2a3ff058188f6
matt <matt@videa.ai>
2025-08-19 22:45:25 -0500

    nix rest of docs

--- docs/tutorials/azure/sql/load_drivers_data.sql
+++
@@ -1,8 +0,0 @@
-COPY INTO dbo.driver_hourly
-FROM 'https://feastonazuredatasamples.blob.core.windows.net/feastdatasamples/driver_hourly.csv'
-WITH
-(
-	FILE_TYPE = 'CSV'
-	,FIRSTROW = 2
-	,MAXERRORS = 0
-)
--- docs/tutorials/azure/sql/load_orders_data.sql
+++
@@ -1,8 +0,0 @@
-COPY INTO dbo.orders
-FROM 'https://feastonazuredatasamples.blob.core.windows.net/feastdatasamples/orders.csv'
-WITH
-(
-	FILE_TYPE = 'CSV'
-	,FIRSTROW = 2
-	,MAXERRORS = 0
-)
